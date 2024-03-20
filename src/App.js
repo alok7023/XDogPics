63768eb05889fa6f6858e2bf78ec964b92d5b113
Alok Nayan <aloknayan@Aloks-MacBook-Air.local>
2024-03-20 15:49:16 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [breadName, setBreadName] = useState("random");
+  const [dogImg, setDogImg] = useState("random");
+  const fetchData = async (value) => {
+    try {
+      let url = "";
+      if (value === "random") url = "https://dog.ceo/api/breeds/image/random";
+      else url = `https://dog.ceo/api/breed/${value}/images/random`;
+
+      const response = await axios.get(url);
+      console.log(response.data.message);
+      setDogImg(response.data.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    fetchData(breadName);
+  }, [breadName]);
+
+  const changeHandler = (e) => {
+    setBreadName(e.target.value);
+  };
+  const nextHandler = (breadName) => {
+    fetchData(breadName);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Show Dog Pics</h1>
+      <label>
+        Select a breed:
+        <select value={breadName} onChange={changeHandler}>
+          <option value="random">Random</option>
+          <option value="beagle">Beagle</option>
+          <option value="boxer">Boxer</option>
+          <option value="dalmation">Dalmatian</option>
+          <option value="husky">Husky</option>
+        </select>
+      </label>
+      <div className="pics">
+        <img src={dogImg} alt="name" />
+      </div>
+      <button onClick={() => nextHandler(breadName)}>next</button>
     </div>
   );
 }
